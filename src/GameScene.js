fc5c3e47413e9ed08d0ad34a9e97a4d38a35b05c
AldoRmzCode <aldoraga0209@gmail.com>
2022-11-01 10:00:35 -0600

    make it better

--- src/GameScene.js
+++ src/GameScene.js
@@ -493,7 +493,7 @@ class GameScene extends Phaser.Scene {
 					doit = setTimeout(resize, 100);
 				  });
 				//go packet
-				var server = this.options.server == "eu1" ? "wss://wargardenz.herokuapp.com" : this.options.server == "us2" ? "wss://wargardenz2.herokuapp.com" : "wss://sword-io-game.herokuapp.com";
+				var server = this.options.server == "eu1" ? "wss://wargardenz-eu1.herokuapp.com" : this.options.server == "us2" ? "wss://wargardenz2.herokuapp.com" : "wss://wargardenz.herokuapp.com";
 				// server = undefined; // Enable for localhost/development
 				function isPrivateIP(ip) {
 					//remove port if present
